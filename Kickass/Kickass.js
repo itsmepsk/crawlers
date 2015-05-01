7808eb18f12a0cfaa49450a7cbc5466f3989f8db
Prathamesh Kakade <itsmepsk@gmail.com>
2015-05-01 13:59:47 +0530

    Update Kickass.js

--- Kickass/Kickass.js
+++ Kickass/Kickass.js
@@ -6,6 +6,8 @@ for(i=0;i<y.length;i++) {
 		if (audioRating !== 0) {
 			y[i].nextSibling.nextSibling.childNodes[1].childNodes[3].value = audioRating;
 			y[i].innerHTML = "Thank You!";
+			y[i].nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.childNodes[0].click();
+			y[i].parentNode.parentNode.childNodes[3].childNodes[1].click();
 		}
 		
 		videoRating  = y[i].nextSibling.nextSibling.childNodes[7].childNodes[0].data; 
@@ -13,12 +15,13 @@ for(i=0;i<y.length;i++) {
 		if (videoRating !== 0) {
 			y[i].nextSibling.nextSibling.childNodes[5].childNodes[3].value = videoRating;
 			y[i].innerHTML = "Thank You!";
+			y[i].nextSibling.nextSibling.childNodes[0].click();
+			y[i].parentNode.parentNode.childNodes[3].childNodes[1].click();
 		}
 		
 	}
 	if (y[i].nextSibling.nextSibling.className === "buttonsline floatleft") {
 		y[i].innerHTML = "Thank You!";
 	}
-	y[i].nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.childNodes[0].click();
-	y[i].parentNode.parentNode.childNodes[3].childNodes[1].click();
+	
 }
