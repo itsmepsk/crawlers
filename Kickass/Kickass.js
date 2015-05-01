58addbe9dd4c45fa4759bd68ad62b451fd162f68
Prathamesh Kakade <itsmepsk@gmail.com>
2015-05-01 13:55:34 +0530

    Create Kickass.js

---
+++ Kickass/Kickass.js
@@ -0,0 +1,24 @@
+y = $('table.data td.nobr textarea.botmarg5px.feedbacktextarea');
+for(i=0;i<y.length;i++) {
+	if(y[i].nextSibling.nextSibling.className === "qrateContainer feedback loggedCondition") {
+		audioRating  = y[i].nextSibling.nextSibling.childNodes[3].childNodes[0].data;
+		audioRating = parseInt(audioRating);
+		if (audioRating !== 0) {
+			y[i].nextSibling.nextSibling.childNodes[1].childNodes[3].value = audioRating;
+			y[i].innerHTML = "Thank You!";
+		}
+		
+		videoRating  = y[i].nextSibling.nextSibling.childNodes[7].childNodes[0].data; 
+		videoRating = parseInt(videoRating);
+		if (videoRating !== 0) {
+			y[i].nextSibling.nextSibling.childNodes[5].childNodes[3].value = videoRating;
+			y[i].innerHTML = "Thank You!";
+		}
+		
+	}
+	if (y[i].nextSibling.nextSibling.className === "buttonsline floatleft") {
+		y[i].innerHTML = "Thank You!";
+	}
+	y[i].nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.childNodes[0].click();
+	y[i].parentNode.parentNode.childNodes[3].childNodes[1].click();
+}
